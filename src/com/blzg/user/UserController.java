e7dbd067ff9759905afe876eb1c640a7f0cee97c
Administrator <yaoyunlong2012@gmail.com>
2016-08-04 14:32:41 +0800

    first commit

---
+++ src/com/blzg/user/UserController.java
@@ -0,0 +1,40 @@
+package com.blzg.user;
+
+import com.demo.common.model.UserInfo;
+import com.jfinal.core.Controller;
+
+/**
+ * BlogController
+ * 所有 sql 与业务逻辑写在 Model 或 Service 中，不要写在 Controller 中，养成好习惯，有利于大型项目的开发与维护
+ */
+public class UserController extends Controller {
+	public void index() {
+		setAttr("userList", UserInfo.me.paginate(getParaToInt("pageNo", 1), 10));
+//		render("blog.html");
+		renderJson();
+	}
+	
+	public void add() {
+	}
+	
+	public void save() {
+		getModel(UserInfo.class).save();
+		redirect("/blog");
+	}
+	
+	public void edit() {
+		setAttr("blog", UserInfo.me.findById(getParaToInt()));
+	}
+	
+	public void update() {
+		getModel(UserInfo.class).update();
+		redirect("/blog");
+	}
+	
+	public void delete() {
+		UserInfo.me.deleteById(getParaToInt());
+		redirect("/blog");
+	}
+}
+
+
